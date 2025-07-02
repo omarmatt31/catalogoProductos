79bc55fc70ff00b51d8843c14af7c3fc7cd691f9
Omar Mattos <omarmatt84@gmail.com>
2025-07-02 20:31:12 -0300

    agregamos validaciones al formulario de login y nos logueamos

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,12 @@ import FormularioProducto from "./components/pages/producto/FormularioProducto";
 import Footer from "./components/shared/Footer";
 import Menu from "./components/shared/Menu";
 import Login from "./components/pages/producto/Login";
+import { useState } from "react";
 
 function App() {
+  const usuarioLogueado = JSON.parse(sessionStorage.getItem('userKey')) || false
+  //no ES NECESARIO EL json.PARSE, xq un valor booleano es un datp valido
+  const [usuarioAdmin, setUsuarioAdmin] = useState(usuarioLogueado)
   return (
     <>
     <BrowserRouter>
@@ -18,7 +22,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Inicio></Inicio>}></Route>
           <Route path="/detalle" element={<DetalleProducto></DetalleProducto>}></Route>
-          <Route path="/login" element={<Login></Login>}></Route>
+          <Route path="/login" element={<Login setUsuarioAdmin={setUsuarioAdmin}></Login>}></Route>
           <Route path="/administrador" element={<Administrador></Administrador>}></Route>
           <Route path="/administrador/crear" element={<FormularioProducto></FormularioProducto>}></Route>
           <Route path="/administrador/editar" element={<FormularioProducto></FormularioProducto>}></Route>
